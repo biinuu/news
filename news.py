63ef8ae7f59fe8160851b6245351eb8cf08a766c
biinuu <binukvs50@gmail.com>
2026-05-21 15:14:05 +0530

    Load news dataset and process date format

--- news.py
+++ news.py
@@ -2,3 +2,9 @@ import pandas as pd
 import psycopg2
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
+
+nltk.download('vader_lexicon')
+
+df = pd.read_csv(r"C:\Users\binuk\Downloads\news_data.csv")
+
+df['Date'] = pd.to_datetime(df['Date'], format='%d-%m-%Y')
