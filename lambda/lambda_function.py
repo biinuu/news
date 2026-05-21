ecb8e4bd6f69b83d0713f35fef3905ff11426b6c
biinuu <binukvs50@gmail.com>
2026-05-21 15:39:10 +0530

    Add Lambda imports and NLP setup

---
+++ lambda/lambda_function.py
@@ -0,0 +1,133 @@
+import os
+import nltk
+import json
+import requests
+import psycopg2
+import boto3
+
+
+nltk.data.path.append("/tmp")
+
+nltk.download("vader_lexicon", download_dir="/tmp")
+
+from nltk.sentiment import SentimentIntensityAnalyzer
+from datetime import datetime
+
+# download vader lexicon
+
+
+# -----------------------------
+# CONFIGURATION
+# -----------------------------
+
+API_KEY = "cc46542c42274b18aa5fc8136dab67aa"
+
+S3_BUCKET = "news-sentiment-raw-data6"
+
+DB_HOST = "news-db1.c01qkasokt8i.us-east-1.rds.amazonaws.com"
+
+DB_NAME = "postgres"
+
+DB_USER = "postgres"
+
+DB_PASS = "News12345"
+
+# -----------------------------
+# AWS S3 CLIENT
+# -----------------------------
+
+s3 = boto3.client('s3')
+
+# -----------------------------
+# MAIN LAMBDA FUNCTION
+# -----------------------------
+
+def lambda_handler(event, context):
+
+    # -------------------------
+    # CALL NEWS API
+    # -------------------------
+
+    url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    # -------------------------
+    # SAVE RAW JSON TO S3
+    # -------------------------
+
+    file_name = f"news_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
+
+    s3.put_object(
+        Bucket=S3_BUCKET,
+        Key=file_name,
+        Body=json.dumps(data)
+    )
+
+    # -------------------------
+    # CONNECT TO POSTGRESQL
+    # -------------------------
+
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+        port="5432",
+        sslmode="require"
+    )
+
+    cur = conn.cursor()
+
+    # -------------------------
+    # SENTIMENT ANALYSIS
+    # -------------------------
+
+    sia = SentimentIntensityAnalyzer()
+
+    for article in data['articles']:
+
+        title = article['title']
+
+        source = article['source']['name']
+
+        published = article['publishedAt']
+
+        score = sia.polarity_scores(title)['compound']
+
+        label = "Positive" if score > 0 else "Negative"
+
+        cur.execute(
+            '''
+            INSERT INTO news_sentiment
+            (
+                news_date,
+                source_name,
+                title,
+                sentiment_score,
+                sentiment_label
+            )
+            VALUES (%s, %s, %s, %s, %s)
+            ''',
+            (
+                published,
+                source,
+                title,
+                score,
+                label
+            )
+        )
+
+    conn.commit()
+
+    cur.close()
+
+    conn.close()
+
+    return {
+        "statusCode": 200,
+        "body": "News inserted successfully!"
+    }
+    
