16ac8df43007db3219258468f1a4b618fec8b41b
biinuu <binukvs50@gmail.com>
2026-05-21 14:47:58 +0530

    Configure News API and sentiment analyzer

--- fetch_news.py
+++ fetch_news.py
@@ -7,49 +7,6 @@ from datetime import datetime
 
 nltk.download('vader_lexicon')
 
-API_KEY = "cc46542c42274b18aa5fc8136dab67aa"
+API_KEY = "your_api_key"
 
 url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}"
-
-response = requests.get(url)
-data = response.json()
-
-articles = data['articles']
-
-sia = SentimentIntensityAnalyzer()
-
-conn = psycopg2.connect(
-    database="newsapi",
-    user="postgres",
-    password="root",
-    host="localhost",
-    port="5432"
-)
-
-cur = conn.cursor()
-
-for article in articles:
-
-    title = article['title']
-    source = article['source']['name']
-    published = article['publishedAt']
-
-    score = sia.polarity_scores(title)['compound']
-
-    label = "Positive" if score > 0 else "Negative"
-
-    cur.execute(
-        """
-        INSERT INTO news_sentiment
-        (news_date, source_name, title, sentiment_score, sentiment_label)
-        VALUES (%s, %s, %s, %s, %s)
-        """,
-        (published, source, title, score, label)
-    )
-
-conn.commit()
-
-cur.close()
-conn.close()
-
-print("News inserted successfully!")
