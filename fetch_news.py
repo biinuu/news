a8a7fd18dbf1c056ff0095707d6d2937a556132f
biinuu <binukvs50@gmail.com>
2026-06-09 12:37:40 +0530

    git commit -m "Configure VADER sentiment analysis"

--- fetch_news.py
+++ fetch_news.py
@@ -4,3 +4,19 @@ import psycopg2
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 from datetime import datetime
+
+response = requests.get(url)
+
+data = response.json()
+
+articles = data['articles']
+
+for article in articles:
+
+    title = article['title']
+    source = article['source']['name']
+    published = article['publishedAt']
+
+    score = sia.polarity_scores(title)['compound']
+
+    label = "Positive" if score > 0 else "Negative"
